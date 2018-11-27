74de6efa32beb5816e76ef87c4f43e7362812505
Asad Khan <asad_khan47@hotmail.co.uk>
2018-11-27 14:16:33 +0000

    First commit

---
+++ src/fizzBuzz.js
@@ -0,0 +1,22 @@
+var FizzBuzz = function() {};
+
+
+FizzBuzz.prototype.play = function (number) {
+  if (this._isDivisibleBy(15, number)) {
+    return 'FizzBuzz';
+  } else if (this._isDivisibleBy(5, number)) {
+    return 'Buzz';
+  } else if (this._isDivisibleBy(3, number)) {
+    return 'Fizz';
+  } else {
+    return number;
+  }
+};
+
+FizzBuzz.prototype._isDivisibleBy = function(divisor, number) {
+  return number % divisor === 0;
+};
+
+for (var i = 1, i <= 100; i++) {
+  console.log(fizzBuzz.play(i));
+};
